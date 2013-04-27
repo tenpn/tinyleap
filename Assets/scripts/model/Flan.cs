ee584a5ac0126b216c83d50133f7f5e8a5c09cd1
Andrew Fray <andrew@spryfox.com>
2013-04-27 23:51:37 +0100

    flans move left and right

--- Assets/scripts/model/Flan.cs
+++ Assets/scripts/model/Flan.cs
@@ -4,8 +4,33 @@ using System;
 
 public class Flan : Building
 {
-    
-    //////////////////////////////////////////////////
+    public override void Tick(int currentColIndex, int flanLaneIndex)
+    {
+        int newCol = currentColIndex + m_direction;
+        if (newCol >= Grid.Instance.ColumnCount)
+        {
+            m_direction = -1;
+            newCol -= 2;
+        }
+        else if (newCol < 0)
+        {
+            m_direction = 1;
+            newCol = 1;
+        }
+
+        Grid.Instance.MoveFlan(flanLaneIndex, currentColIndex, newCol);
+    }   
+
+    public bool IsGoingRight { get { return m_direction > 0; } }
 
     //////////////////////////////////////////////////
+
+    private int m_direction = 1;
+
+    //////////////////////////////////////////////////
+    
+    private void OnSpawn()
+    {
+        m_direction = 1;
+    }
 }
