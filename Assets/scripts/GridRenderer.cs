0e0cdcddf63a4fbe68a323ed0133984abfb8564d
Andrew Fray <andrew@spryfox.com>
2013-04-27 11:36:07 +0100

    use ranged attributes on grid render properties

--- Assets/scripts/GridRenderer.cs
+++ Assets/scripts/GridRenderer.cs
@@ -6,6 +6,11 @@ public class GridRenderer : MonoBehaviour
 {
     //////////////////////////////////////////////////
 
+    [RangeAttribute(0.0f, 1.0f)]
+    [SerializeField] private float m_gridToScreenWidth = 0.9f;
+    [RangeAttribute(0.0f, 1.0f)]
+    [SerializeField] private float m_gridToScreenHeight = 0.9f;
+
     //////////////////////////////////////////////////
 
     private void Update()
