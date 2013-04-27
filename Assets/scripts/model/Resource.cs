828cb97dc3acdd5a2e5a351f216bfe3a4089f870
Andrew Fray <andrew@spryfox.com>
2013-04-28 00:19:23 +0100

    resources is base class for all resource types
    just derive to create a new type, there's nothing to override. uses typename of your leaf class.

---
+++ Assets/scripts/model/Resource.cs
@@ -0,0 +1,22 @@
+
+using System;
+using UnityEngine;
+
+public abstract class Resource : Building
+{
+    public string ResourceType { get { return GetType().ToString(); } }
+
+    public int Count { get { return m_count; } }
+
+    //////////////////////////////////////////////////
+
+    [SerializeField] private int m_count;
+    [SerializeField] private int m_capacity;
+
+    //////////////////////////////////////////////////
+
+    private void OnSpawn()
+    {
+        m_count = m_capacity;
+    }
+}
