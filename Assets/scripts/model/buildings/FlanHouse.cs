e74da84758da9266a4f662896057f2b09e7ec2b3
Andrew Fray <andrew@spryfox.com>
2013-04-27 23:30:46 +0100

    flan houses make flans if they don't have one already

--- Assets/scripts/model/buildings/FlanHouse.cs
+++ Assets/scripts/model/buildings/FlanHouse.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class FlanHouse : Building
 {
-    
-    //////////////////////////////////////////////////
+    public override void Tick(int colIndex, int flanLaneIndex)
+    {
+        if (m_myFlan == null)
+        {
+            Debug.Log("making flan!");
+            m_myFlan = Grid.Instance.CreateFlan(colIndex, flanLaneIndex);
+        }
+    }
 
     //////////////////////////////////////////////////
+    
+    private Flan m_myFlan = null;
+
+    //////////////////////////////////////////////////
+
+    private void OnDespawn()
+    {
+        m_myFlan = null;
+    }
 }
