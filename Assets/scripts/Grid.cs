9e1af335cba9f089758602ef34bc9b62806d320f
Andrew Fray <andrew@spryfox.com>
2013-04-27 11:35:59 +0100

    use ranged attributes on grid properties

--- Assets/scripts/Grid.cs
+++ Assets/scripts/Grid.cs
@@ -7,7 +7,9 @@ public class Grid : MonoSingleton<Grid>
 
     //////////////////////////////////////////////////
 
+    [RangeAttribute(1, 50)]
     [SerializeField] private int m_columns;
+    [RangeAttribute(1, 10)]
     [SerializeField] private int m_flanLanes; 
 
     //////////////////////////////////////////////////
