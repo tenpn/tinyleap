859334578ded49ecb9a35c9f90eeca511c6ffc3f
Andrew Fray <andrew@spryfox.com>
2013-04-27 11:29:27 +0100

    grid class!

---
+++ Assets/scripts/Grid.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+using System;
+
+public class Grid : MonoSingleton<Grid>
+{    
+
+    //////////////////////////////////////////////////
+
+    [SerializeField] private int m_columns;
+    [SerializeField] private int m_flanLanes; 
+
+    //////////////////////////////////////////////////
+
+}
