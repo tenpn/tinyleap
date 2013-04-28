674910e0c7845831f5d445aed67a45f467ef8cf5
Andrew Fray <andrew@spryfox.com>
2013-04-28 20:52:46 +0100

    building factory derives from the generalised version

--- Assets/scripts/model/buildings/BuildingFactory.cs
+++ Assets/scripts/model/buildings/BuildingFactory.cs
@@ -3,57 +3,7 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 
-public class BuildingFactory : MonoBehaviour
+public class BuildingFactory : Factory<Building>
 {
-    public T Create<T>() where T : Building
-    {
-        return Create(typeof(T)) as T;
-    }
-
-    public Building Create(Type buildingType)
-    {
-        Assert.True(m_spawnPools.ContainsKey(buildingType),
-                    "building " + buildingType + " not found in factory");
-        var pool = m_spawnPools[buildingType];
-        return pool.Spawn();
-    }
-
-    public void Destroy(Building b)
-    {
-        if (b == null)
-        {
-            return;
-        }
-
-        var buildingType = b.GetType();
-        Assert.True(m_spawnPools.ContainsKey(buildingType),
-                    "building " + buildingType + " not found in factory");
-        m_spawnPools[buildingType].Despawn(b);
-    }
-
-    public IEnumerable<Type> AllBuildingTypes { get { return m_spawnPools.Keys; } }
-
-    //////////////////////////////////////////////////
-
-    private Dictionary<Type, SpawnPool<Building>> m_spawnPools
-        = new Dictionary<Type, SpawnPool<Building>>();
-
-    //////////////////////////////////////////////////
-
-    private void Awake()
-    {
-        var spawnBuildingStore = new GameObject("pool").transform;
-        spawnBuildingStore.parent = transform;
-
-        var allBuildings = GetComponentsInChildren<Building>();
-
-        foreach(var building in allBuildings)
-        {
-            Assert.True(m_spawnPools.ContainsKey(building.GetType()) == false,
-                        "building " + building.gameObject + " found twice in prototype list");
-            m_spawnPools[building.GetType()] 
-                = new SpawnPool<Building>(building.gameObject, spawnBuildingStore);
-        }
-    }
 
 }
