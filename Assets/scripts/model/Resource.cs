968cf7bda49194dd5bc9d94754efce9a81cbfa77
Andrew Fray <andrew@spryfox.com>
2013-04-28 19:20:02 +0100

    resource event for when one has been picked up

--- Assets/scripts/model/Resource.cs
+++ Assets/scripts/model/Resource.cs
@@ -9,6 +9,13 @@ public abstract class Resource : Building
     public int Count { get { return m_count; } }
     public float PercentageRemaining { get { return m_count / (float)m_capacity; } }
 
+    public void OnPickup()
+    {
+        Assert.True(m_count > 0,
+                    "cannot pick up resource when have 0 count");
+        --m_count;
+    }
+
     //////////////////////////////////////////////////
 
     private int m_count;
