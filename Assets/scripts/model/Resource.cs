6e4a23d669d3ac9f16ec112b73dde4d6f31e8cf1
Andrew Fray <andrew@spryfox.com>
2013-04-28 18:47:53 +0100

    resource can be queried for % remaining

--- Assets/scripts/model/Resource.cs
+++ Assets/scripts/model/Resource.cs
@@ -7,6 +7,7 @@ public abstract class Resource : Building
     public string ResourceType { get { return GetType().ToString(); } }
 
     public int Count { get { return m_count; } }
+    public float PercentageRemaining { get { return m_count / (float)m_capacity; } }
 
     //////////////////////////////////////////////////
 
